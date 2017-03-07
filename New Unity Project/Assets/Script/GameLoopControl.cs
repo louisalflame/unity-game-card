f3aae84f40c0f7bfb4f5a740c461c1f1c586da72
louisalflame <louisalflame@gmail.com>
2017-03-08 01:24:44 +0800

    main loop
    create basic main loop

---
+++ New Unity Project/Assets/Script/GameLoopControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoopControl : MonoBehaviour {
+
+    SceneController sceneController = new SceneController();
+
+    void awake(){
+        // 切換場景不會被刪除
+        GameObject.DontDestroyOnLoad( this.gameObject );
+    }
+
+	void Start () {
+        // 設定起始scene
+        // sceneController.setScene( new MenuScene( sceneController ) );
+	}
+	
+	// 所有更新的起點
+	void Update () {
+        // 由SceneControl負責場景下的更新
+        sceneController.sceneUpdate();
+	}
+
+
+}
---
+++ New Unity Project/Assets/Script/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController {
+     
+    // private sceneState state;
+
+    public SceneController() { }
+	
+	// 場景下所有的更新
+	void sceneUpdate () {
+    }
+
+    // 圖片的移動
+    void FixedUpdate() {
+    }
+
+    //玩家輸入
+    void InputProcess() {
+    }
+}
