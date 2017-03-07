b099c43190ad5ed322ee3fe27bf7d94fbee52c45
louisalflame <louisalflame@gmail.com>
2017-03-08 00:15:00 +0800

    new
    add new unity project

---
+++ New Unity Project/Assets/Script/GameDiceManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameDiceManager : MonoBehaviour {
+
+    public enum Process
+    {
+        prepare,
+        mov_prepare_dice,
+        mov_pop_dice,
+        mov_throw_dice,
+        mov_arrange,
+        mov_collect_build,
+        mov_skill,
+        atk_arrange,
+        def_arrange,
+        atk_skill,
+        def_skill,        
+        turn_conclusion
+    }
+
+    public Process process;
+
+	// Use this for initialization
+	void Start () {
+        process = Process.prepare;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (process)
+        {
+            case Process.prepare:
+                Debug.Log("prepare");
+                process = Process.mov_prepare_dice;
+                break;
+            case Process.mov_prepare_dice:
+                break;
+        }
+	}
+}
---
+++ New Unity Project/Assets/Script/InstantiateTry.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateTry : MonoBehaviour
+{
+
+    public GameObject dice;
+    public GameObject newdice;
+
+	// Use this for initialization
+	void Start () {
+        newdice = Instantiate(dice, new Vector3(0,0,0), new Quaternion(0, 0, 0, 0)) as GameObject;
+        Instantiate(dice, new Vector3(0.2f, 0.2f, 0), new Quaternion(0, 0, 0, 0));
+
+        newdice.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        Debug.Log("created "+newdice.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
